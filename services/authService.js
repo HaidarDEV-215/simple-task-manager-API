f4661887e78781aa040c9437c0556a432d420a3c
Haidar Shawish <haidarshawishnl@gmail.com>
2026-09-21 20:09:40 +0300

    formatting auth service

--- services/authService.js
+++ services/authService.js
@@ -13,7 +13,6 @@ const regesterService = async (firstName, lastName, email, password) => {
     if (existUser) {
         throw new AppError(`User with email ${email} already exists`, 400, statusText.FAIL);
     }
-
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUserData = {
         firstName,
@@ -21,10 +20,8 @@ const regesterService = async (firstName, lastName, email, password) => {
         email,
         password: hashedPassword
     };
-
     const newUser = new User(newUserData);
     await newUser.save();
-
     const token = await createToken({ email: email, id: newUser._id, isAdmin: newUser.isAdmin });
     return token;
 }
@@ -33,19 +30,15 @@ const loginService = async (email, password) => {
     if (!email || !password) {
         throw new AppError(`Email and password are required`, 400, statusText.FAIL);
     }
-
     const existUser = await User.findOne({ email });
     if (!existUser) {
         throw new AppError(`Invalid email or password`, 400, statusText.FAIL);
     }
-
     const matchedPassword = await bcrypt.compare(password, existUser.password);
     if (!matchedPassword) {
         throw new AppError(`Invalid email or password`, 400, statusText.FAIL);
     }
-
     const token = await createToken({ email: email, id: existUser._id, isAdmin: existUser.isAdmin });
-
     return token;
 }
 
@@ -53,28 +46,21 @@ const forgetPasswordService = async (email) => {
     if (!email) {
         throw new AppError(`Email is required`, 400, statusText.FAIL);
     }
-
     const user = await User.findOne({ email });
     if (!user) {
         throw new AppError(`User with this email does not exist!`, 404, statusText.FAIL);
     }
-
     await Otp.deleteMany({ email });
-
     const OneTimePassword = crypto.randomInt(100000, 999999);
-
     const otpData = {
         email: email,
         user: user._id,
         code: OneTimePassword,
         expiresAt: Date.now() + (1000 * 60 * 5) // 5 mins
     };
-
     const otp = new Otp(otpData);
     await otp.save();
-
     await emailService(email, 'Verify your account', verificationEmail(OneTimePassword));
-
     return true;
 }
 
@@ -82,22 +68,17 @@ const confirmOTPservice = async (email, code) => {
     if (!email || !code) {
         throw new AppError("Email and code are required!", 400, statusText.FAIL);
     }
-
     const otp = await Otp.findOne({ email: email, code: code });
     if (!otp) {
         throw new AppError('Invalid OTP!', 400, statusText.FAIL);
     }
-
     if (otp.expiresAt < Date.now()) {
         await Otp.deleteOne({ _id: otp._id });
         throw new AppError('OTP has expired!', 400, statusText.FAIL);
     }
-
     //delete otp when its done
     await Otp.deleteOne({ _id: otp._id });
-
     const resetPasswordToken = await createToken({ id: otp.user, email: otp.email, purpose: "reset-password" }, "5m");
-    
     return resetPasswordToken;
 }
 
@@ -105,18 +86,11 @@ const resetPasswordService = async (newPassword, email) => {
     if (!newPassword) {
         throw new AppError("Password is required!", 400, statusText.FAIL);
     }
-
     const hashedPassword = await bcrypt.hash(newPassword, 10);
     console.log(hashedPassword);
     await User.findOneAndUpdate({ email }, { password: hashedPassword }, { runValidators: true });
-
     return true;
 }
 
-module.exports = {
-    regesterService,
-    loginService,
-    forgetPasswordService,
-    confirmOTPservice,
-    resetPasswordService
+module.exports = {regesterService, loginService, forgetPasswordService, confirmOTPservice, resetPasswordService
 };
